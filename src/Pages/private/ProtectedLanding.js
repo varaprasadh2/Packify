1732a69576835af1125537542b0c395d605b93aa
Varaprasadh <90190222+varaprasadh2@users.noreply.github.com>
2023-02-24 18:04:19 +0530

    Delete src/Pages directory

--- src/Pages/private/ProtectedLanding.js
+++
@@ -1,8 +0,0 @@
-import { Navigate,Outlet } from "react-router";
-
-const ProtectedLanding = () =>{
-    const token = localStorage.getItem("Token")
-    return token?<Navigate to ="/packaing_wizard" />  :<Outlet /> ;
-};
-
-export default ProtectedLanding;
