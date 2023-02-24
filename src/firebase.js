ed6fcb927ed70e888e455dd1fa9a0f1aac9c301e
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-02-24 19:59:50 +0530

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -37,16 +37,16 @@ export const signInWithGoogle = async () => {
     try {
         const res = await signInWithPopup(auth, googleProvider);
         const user = res.user;
-        // const q = query(collection(db, "users"), where("uid", "==", user.uid));
-        // const docs = await getDocs(q);
-        // if (docs.docs.length === 0) {
-        //     await addDoc(collection(db, "users"), {
-        //         uid: user.uid,
-        //         name: user.displayName,
-        //         authProvider: "google",
-        //         email: user.email,
-        //     });
-        // }
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+        if (docs.docs.length === 0) {
+            await addDoc(collection(db, "users"), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email,
+            });
+        }
         return user;
     } catch (err) {
         console.error(err);
