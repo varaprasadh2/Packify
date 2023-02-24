aa857c51d8f4e9ffe83bb52bcf915e1e3e5f47d3
Pavan0369 <pavankumar.k@imaginnovate.com>
2023-02-24 17:09:18 +0530

    protected routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router ,Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
+import packaing_wizard from './Pages/PackingWizard';
+import ProtectedDashboard from './Pages/private/ProtectedDashboard';
+import ProtectedLanding from './Pages/private/ProtectedLanding';
+
+
+
 
 function App() {
   return (
-    <LandingPage />
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route element={<ProtectedLanding/>}>
+            <Route path='' element={<LandingPage />}/>
+          </Route>
+          <Route element={<ProtectedDashboard/>}>
+            <Route path='/packaing_wizard' element={<LandingPage />}/>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
