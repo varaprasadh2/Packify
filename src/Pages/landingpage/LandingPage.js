f03179f455983c1f977b7df7e243608218beb2fa
Yogesh-S98 <109652643+Yogesh-S98@users.noreply.github.com>
2023-02-24 19:57:26 +0530

    changes

--- src/Pages/landingpage/LandingPage.js
+++ src/Pages/landingpage/LandingPage.js
@@ -16,11 +16,7 @@ const landingPage = () => {
             <div className="sub-heading">
                 Efficiently pack your items into any container size or shape with <span className="sub-company">Packify</span>, the ultimate load planning solution. Our platform uses advanced algorithms to revolutionize your load planning process, allowing you to maximize space while minimizing the risk of damage during transport. Say goodbye to inefficient load planning and hello to streamlined logistics with <span className="sub-company">Packify</span>.
             </div>
-            <div className="button-div">
-            <div className="button">
-                <img width={15} height={15} src={Google} className="google-logo"/> Continue with google
-            </div>
-            </div>
+            <div className="button btn"><img width={15} src={Google}/> Continue with google</div>
         </div>
         
       </div>
