0f44c7a880f3883055f6b6059a7e0a6f6b9700f7
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-02-25 12:38:24 +0530

    add boilderplate

---
+++ src/pages/private/LoadPlan/Components/Container/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Container({ name, width, height, depth, weight, maxWeight, quantity, onDelete = () => { } }) {
+
+    return (
+        <div>
+            {/* input fields */}
+            {/* delete icon */}
+        </div>
+    );
+
+}
---
+++ src/pages/private/LoadPlan/Components/Item/Item.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function Item({ name, width, height, depth, weight, quantity, onDelete = () => {} })  {
+
+    return (
+        <div>
+            {/* input fields */}
+            {/* delete icon */}
+        </div>
+    );
+
+}
