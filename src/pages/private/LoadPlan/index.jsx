524d50ea439515b324413f4afe7f8bc61e0deeb7
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-02-25 16:27:55 +0530

    sfdsgs

--- src/pages/private/LoadPlan/index.jsx
+++ src/pages/private/LoadPlan/index.jsx
@@ -4,32 +4,25 @@ import { useNavigate } from "react-router-dom";
 import LoadPlanForm from './Components/LoadPlanForm/Index';
 import "./index.css";
 
-
-
-function Content() {
+export default function LoadPlan() {
     const navigate = useNavigate();
     const dashboard = () => {
         navigate("/dashboard");
     }
-    return (
-        <div className="load_content">
-            <div className='load_content_top'>
-                <i className="arrow_icon fa fa-long-arrow-left" onClick={dashboard}></i>
-                <div className='lct_right'>
-                    <div className='lct_heading'>Load Plan</div>
-                    {/* <Button type="primary" icon={<DownloadOutlined />}></Button> */}
-                </div>
-            </div>
-            <LoadPlanForm />
-        </div>
-    )
-}
 
-export default function LoadPlan() {
     return (
         <div>
             <NavBar/>
-            <Content/>
+            <div className="load_content">
+                <div className='load_content_top'>
+                    <i className="arrow_icon fa fa-long-arrow-left" onClick={dashboard}></i>
+                    <div className='lct_right'>
+                        <div className='lct_heading'>Load Plan</div>
+                        {/* <Button type="primary" icon={<DownloadOutlined />}></Button> */}
+                    </div>
+                </div>
+                <LoadPlanForm />
+            </div>
         </div>
     )
 }
