0b4b6b1f7e652046f49639d0b2da77ceeeebeeaa
Pavan0369 <pavankumar.k@imaginnovate.com>
2023-02-25 19:16:46 +0530

    fjsakdfjdsa

--- src/pages/private/LoadPlan/Components/Report/Summary/Summary.js
+++ src/pages/private/LoadPlan/Components/Report/Summary/Summary.js
@@ -2,6 +2,34 @@ import "./Summary.css";
 
 export default function Summary({}){
     return (
-        <div>Overall Summary</div>
+        <div className="summary">
+            <div style={{marginBottom:'20px'}}>Overall Summary</div>
+            <div className="overall_list">
+                <div className="overall_list_item">
+                    <div>Items Packed</div>
+                    <div>75</div>
+                </div>
+                <div className="overall_list_item">
+                    <div>Items not Packed</div>
+                    <div>75</div>
+                </div>
+                <div className="overall_list_item">
+                    <div>Total Volume</div>
+                    <div>75</div>
+                </div>
+                <div className="overall_list_item">
+                    <div>Used Volume</div>
+                    <div>75</div>
+                </div>
+                <div className="overall_list_item">
+                    <div>Weight</div>
+                    <div>75</div>
+                </div>
+                <div className="overall_list_item_last">
+                    <div>Net Weight</div>
+                    <div>75</div>
+                </div>
+            </div>
+        </div>
     )
 }
