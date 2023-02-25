d850f828893a5f88c0524daba72c16061292dd6c
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-02-25 21:11:18 +0530

    sfdjs

--- src/pages/private/LoadPlan/index.jsx
+++ src/pages/private/LoadPlan/index.jsx
@@ -5,16 +5,19 @@ import LoadPlanForm from './Components/LoadPlanForm/Index';
 import "./index.css";
 import { packBins } from "../../../utils";
 import Report from "./Components/Report";
+import { useState } from "react";
 
 export default function LoadPlan() {
     const navigate = useNavigate();
+    const [report, setReport] = useState({});
     const dashboard = () => {
         navigate("/dashboard");
     }
 
     const generateReport = ({ items, containers }) => {
         console.log('generate report', { items, containers });
-        packBins({ items, containers });
+        const result = packBins({ items, containers });
+        setReport(result);
     }
 
     return (
@@ -32,7 +35,7 @@ export default function LoadPlan() {
                     <LoadPlanForm generateReport={generateReport} />
                 </div>
                 <div >
-                    <Report/>
+                    <Report report={report}/>
                 </div>
             </div>
         </div>
