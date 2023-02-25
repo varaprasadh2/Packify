bb6df2a537eb027dc40ff4fff74162b286016f6a
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-02-25 12:29:12 +0530

    logout action

--- src/pages/private/Dashboard/navbar/Navbar.jsx
+++ src/pages/private/Dashboard/navbar/Navbar.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
+import { Menu } from 'antd';
 import './Navbar.css';
 import Mylogo from '../../../../../src/assets/app_logo.png';
 import { logout,auth } from '../../../../firebase';
 import { useAuthState } from "react-firebase-hooks/auth";
+const { Item } = Menu;
 
 export default function NavBar() {
     const [user, loading, error] = useAuthState(auth);
-    const [logoutAccess, setlogoutAccess] = useState(false);
-    const logoutValid = () => {
-        setlogoutAccess((current) => !current);
-    };
+    const [menu, showMenu] = useState(false);
     return (
         <div>
             <div className="nav">
-            <div className="nav_logo">
-                <img src={Mylogo} width='120'></img>
-            </div>
-            <div className="nav-buttons">
-            <div className='nav_right' onClick={logoutValid}>
-                <div>{user?user.displayName:'User Name'}</div>
-                <img src={user.photoURL} alt="Avatar" class="avatar"></img>
-            </div>
-            {logoutAccess && <div className="logout-btn" onClick={logout}>logout</div>}
-            </div>
+                <div className="nav_logo">
+                    <img src={Mylogo} width='120'></img>
+                </div>
+                <div className="nav-buttons">
+                    <div className='nav_right' onClick={() => showMenu(!menu)}>
+                        <div>{user?user.displayName:'User Name'}</div>
+                        <img src={user.photoURL} alt="Avatar" class="avatar"></img>
+                    </div>
+                    {
+                        menu && <div className='actions'>
+                            <div className="action">Logout</div>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
     )
