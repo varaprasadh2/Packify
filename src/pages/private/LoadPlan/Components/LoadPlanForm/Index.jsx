8d0ecf1344d515e27b05a8b2a7e4f5c1c95bb46c
varapradadh2 <varaprasadh.a@imgannovate.com>
2023-02-25 12:40:25 +0530

    new changes

---
+++ src/pages/private/LoadPlan/Components/LoadPlanForm/Index.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+export default function LoadPlanForm(props) {
+    const [items, setItems] = useState([]);
+    const [container, setContainers] = useState([]);
+
+    return (
+        <div>
+            {/* render containers */}
+            {/* render items */}
+            {/* render process button */}
+        </div>
+    );
+
+}
