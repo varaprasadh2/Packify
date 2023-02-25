37afb6ab096da2834a45475af452c2fb5fe40578
UDAY-BHASKAR1 <udaybhaskarambati@gmail.com>
2023-02-25 14:15:26 +0530

    sdfs

--- src/pages/private/Dashboard/navbar/Navbar.jsx
+++ src/pages/private/Dashboard/navbar/Navbar.jsx
@@ -16,18 +16,7 @@ export default function NavBar() {
                 <img src={user.photoURL} alt="Avatar" class="avatar"></img>
                 <i className="sign_out fa fa-sign-out" onClick={logout}></i>
             </div>
-            {/* <div className='wrap'>
-                <img className='image truck-img' src={Mylogo} alt="" />
-                <img className='image box-img' src={parcel} alt="" />
-                <img className='image box-img box-img2' src={parcel} alt="" />
-                <img className='image box-img box-img3' src={parcel} alt="" />
-                <img className='image box-img box-img4' src={parcel} alt="" />
-                <img className='image box-img box-img5' src={parcel} alt="" />
-                <img className='image box-img box-img6' src={parcel} alt="" />
-                <img className='image box-img box-img7' src={parcel} alt="" />
-                <img className='image box-img box-img8' src={parcel} alt="" />
-                <img className='image box-img box-img9' src={parcel} alt="" />
-            </div> */}
+            
         </div>
     )
 }
