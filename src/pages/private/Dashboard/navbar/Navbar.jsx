27ef3cbc2827672e6e98b882586a3bfa3e0172f2
Yogesh-S98 <109652643+Yogesh-S98@users.noreply.github.com>
2023-02-25 11:56:01 +0530

    nav-bar-logout

--- src/pages/private/Dashboard/navbar/Navbar.jsx
+++ src/pages/private/Dashboard/navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from 'react';
 import './Navbar.css';
 import Mylogo from '../../../../../src/assets/app_logo.png';
 import { logout,auth } from '../../../../firebase';
@@ -5,15 +7,23 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function NavBar() {
     const [user, loading, error] = useAuthState(auth);
+    const [logoutAccess, setlogoutAccess] = useState(false);
+    const logoutValid = () => {
+        setlogoutAccess((current) => !current);
+    };
     return (
-        <div className="nav">
-            <div>
-                <img className='nav_logo' src={Mylogo} width='120'></img>
+        <div>
+            <div className="nav">
+            <div className="nav_logo">
+                <img src={Mylogo} width='120'></img>
             </div>
-            <div className='nav_right'>
+            <div className="nav-buttons">
+            <div className='nav_right' onClick={logoutValid}>
                 <div>{user?user.displayName:'User Name'}</div>
                 <img src={user.photoURL} alt="Avatar" class="avatar"></img>
-                <i className="sign_out fa fa-sign-out" onClick={logout}></i>
+            </div>
+            {logoutAccess && <div className="logout-btn" onClick={logout}>logout</div>}
+            </div>
             </div>
         </div>
     )
